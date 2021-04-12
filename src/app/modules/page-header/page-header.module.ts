f244a2e2aa8402287b77ecc9bb0d22994e3661fb
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-04-13 09:42:52 +1000

    Icons added

--- src/app/modules/page-header/page-header.module.ts
+++ src/app/modules/page-header/page-header.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from './page-header.component';
+import {AngularSvgIconModule} from 'angular-svg-icon';
+import {HttpClientModule} from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [PageHeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    AngularSvgIconModule
   ],
   exports: [
     PageHeaderComponent
