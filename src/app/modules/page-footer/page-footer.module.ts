33cdea6e3b2b0630c8fff0819dda8bed7bf72587
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-04-13 23:32:44 +1000

    Page footer added

--- src/app/modules/page-footer/page-footer.module.ts
+++ src/app/modules/page-footer/page-footer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageFooterComponent } from './page-footer.component';
+import {SharedModule} from '../../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [PageFooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     PageFooterComponent
