b80491f55a7de457a40da3f67d5c8108a64ddfc4
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-04-15 11:49:10 +1000

    Page-header added

--- src/app/modules/page-header/page-header.module.ts
+++ src/app/modules/page-header/page-header.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from './page-header.component';
 import {SharedModule} from '../../shared/shared.module';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { MobileNavComponent } from './components/mobile-nav/mobile-nav.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { LocationSelectorComponent } from './components/location-selector/location-selector.component';
 
 @NgModule({
-  declarations: [PageHeaderComponent],
+  declarations: [PageHeaderComponent, MainNavComponent, MobileNavComponent, HeaderUserComponent, BreadcrumbsComponent, LocationSelectorComponent],
   imports: [
     CommonModule,
     SharedModule
