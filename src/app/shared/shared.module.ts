d52da3c649a3566d96845e37e5f45fd966e6db32
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-04-15 12:10:55 +1000

    Select added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,11 @@ import { BtnComponent } from './components/btn/btn.component';
 import { LinkComponent } from './components/link/link.component';
 import { TextBoxComponent } from './components/text-box/text-box.component';
 import { ImgComponent } from './components/img/img.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { SelectComponent } from './components/select/select.component';
+import { SwitcherComponent } from './components/switcher/switcher.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 
@@ -13,12 +18,16 @@ import { ImgComponent } from './components/img/img.component';
   imports: [
     // CommonModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AngularSvgIconModule,
   ],
   exports: [
     // CommonModule,
     HttpClientModule,
     AngularSvgIconModule,
+    FormsModule,
+    ReactiveFormsModule,
     BtnComponent,
     LinkComponent,
     TextBoxComponent,
@@ -28,7 +37,11 @@ import { ImgComponent } from './components/img/img.component';
     BtnComponent,
     LinkComponent,
     TextBoxComponent,
-    ImgComponent
+    ImgComponent,
+    CheckboxComponent,
+    DatepickerComponent,
+    SelectComponent,
+    SwitcherComponent
   ]
 })
 export class SharedModule { }
