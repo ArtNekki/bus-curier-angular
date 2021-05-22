b30a2157f7be317f038d5aa4b9176949d58fc9f8
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-23 07:23:01 +1000

    HEader is sticky now

--- src/app/modules/page-header/page-header.component.ts
+++ src/app/modules/page-header/page-header.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss']
 })
-export class PageHeaderComponent implements OnInit {
+export class PageHeaderComponent implements OnInit, AfterViewInit {
+  @ViewChild('header', {read: ElementRef}) header: ElementRef;
+  isSticky = false;
 
   constructor() { }
 
   ngOnInit() {
+    if (this.isSticky) {
+      document.documentElement.classList.add('page--header-sticky');
+    } else {
+      document.documentElement.classList.remove('page--header-sticky');
+    }
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.setStickyMenu();
+    });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+
+  scroll() {
+    this.setStickyMenu();
+  }
+
+  setStickyMenu() {
+    if (this.header.nativeElement.getBoundingClientRect().top < -30) {
+      this.isSticky = true;
+      // document.documentElement.classList.add('page--header-sticky');
+    } else {
+      this.isSticky = false;
+      // document.documentElement.classList.remove('page--header-sticky');
+    }
+  }
 }
