4cff1e005814785e58e12845ea6a62154aeec0be
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-23 08:07:21 +1000

    Header user refactored

--- src/app/modules/page-header/components/header-user/header-user.component.ts
+++ src/app/modules/page-header/components/header-user/header-user.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-header-user',
   templateUrl: './header-user.component.html',
-  styleUrls: ['./header-user.component.scss']
+  styleUrls: ['./header-user.component.scss'],
+  animations: [
+    trigger('dropdown', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate('200ms')
+      ])
+    ])
+  ]
 })
 export class HeaderUserComponent implements OnInit {
+
+  public isOpen = false;
+  public loggedIn = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+
+    console.log('toggle', this.isOpen);
   }
 
 }
