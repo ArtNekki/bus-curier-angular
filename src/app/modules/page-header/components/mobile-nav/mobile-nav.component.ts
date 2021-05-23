7559f64f981333385dbb7e09051d36ca3734812f
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-05-23 10:17:37 +1000

    Mobile' nav animation refactored

--- src/app/modules/page-header/components/mobile-nav/mobile-nav.component.ts
+++ src/app/modules/page-header/components/mobile-nav/mobile-nav.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import mainNav from '../../../../main-nav';
-import {state, style, trigger} from '@angular/animations';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-mobile-nav',
@@ -10,6 +10,12 @@ import {state, style, trigger} from '@angular/animations';
     trigger('navBlock', [
       state('false', style({transform: 'translateX(100%)', opacity: 0})),
       state('true', style({transform: 'translateX(0)', opacity: 1 })),
+      transition('false => true', animate(200, keyframes([
+        style({ transform: 'translateX(0)', opacity: 1})
+      ]))),
+      transition('true => false', animate(200, keyframes([
+        style({ transform: 'translateX(100%)', opacity: 0})
+      ]))),
     ])
   ]
 })
