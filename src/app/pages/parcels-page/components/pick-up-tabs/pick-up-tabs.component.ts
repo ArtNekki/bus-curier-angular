235b78ea6d4b057ec027b0852a3dbcea4fc742c9
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-06-15 09:16:06 +1000

    Pickup refactored

--- src/app/pages/parcels-page/components/pick-up-tabs/pick-up-tabs.component.ts
+++ src/app/pages/parcels-page/components/pick-up-tabs/pick-up-tabs.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import pickupPoints from '../../../../mock-data/pickup-points';
+import dropdown from '../../../../core/animations/dropdown';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-pick-up-tabs',
   templateUrl: './pick-up-tabs.component.html',
-  styleUrls: ['./pick-up-tabs.component.scss']
+  styleUrls: ['./pick-up-tabs.component.scss'],
+  animations: [
+    trigger('activeTab', [
+      transition('void => *', [
+        // style({opacity: 0, height: '0px'}),
+        style({height: '0px'}),
+        animate('300ms')
+      ])
+    ])
+  ]
 })
 export class PickUpTabsComponent implements OnInit {
   public tabsMap = {
@@ -24,5 +35,9 @@ export class PickUpTabsComponent implements OnInit {
 
   showTab(tab) {
     this.activeTab = tab;
+  }
+
+  setIcon(point: any) {
+    return `assets/img/svg/${this.activeTab === point ? 'caret-up' : 'caret-down'}.svg`;
   }
 }
--- src/app/pages/parcels-page/components/pickup/pickup.component.ts
+++ src/app/pages/parcels-page/components/pickup/pickup.component.ts
@@ -1,16 +1,36 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-pickup',
   templateUrl: './pickup.component.html',
-  styleUrls: ['./pickup.component.scss']
+  styleUrls: ['./pickup.component.scss'],
+  animations: [
+    trigger('activePanel', [
+      transition('void => *', [
+        // style({opacity: 0, height: '0px'}),
+        style({height: '0px'}),
+        animate('300ms')
+      ])
+    ])
+  ]
 })
 export class PickupComponent implements OnInit {
   @Input() data;
 
+  public activePanel = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.activePanel = this.data[0];
   }
 
+  showContacts(data) {
+    this.activePanel = data;
+  }
+
+  setIcon(city: string) {
+    return `assets/img/svg/${this.activePanel === city ? 'minus' : 'plus'}.svg`;
+  }
 }
