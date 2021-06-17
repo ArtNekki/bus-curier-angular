0162ea16c7323b16449679c90f69bf2348131581
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-06-17 16:43:22 +1000

    Radio added

--- src/app/shared/components/radio/radio.component.ts
+++ src/app/shared/components/radio/radio.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ModsService} from '../../../core/services/mods.service';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-radio',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.scss']
+  styleUrls: ['./radio.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => RadioComponent),
+    multi: true
+  }]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements ControlValueAccessor, OnInit {
+  @Input() checked: boolean;
+  @Input() id: string;
+  @Input() mods;
 
-  constructor() { }
+  public cssClass;
+  value = false;
+
+  constructor(private modsService: ModsService) { }
 
   ngOnInit(): void {
+    this.cssClass = this.modsService.setMods('radio', this.mods);
   }
 
+  onChange: (_: any) => void = (_: any) => {};
+  onTouched: () => void = () => {};
+
+  changeValue(value) {
+    this.writeValue(value);
+    // this.change.emit(value);
+  }
+
+  writeValue(value: boolean): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
 }
