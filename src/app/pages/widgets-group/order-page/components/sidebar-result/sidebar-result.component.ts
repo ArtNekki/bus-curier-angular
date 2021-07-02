1e1813669d8272f415cc3b54f2faaedf8688eeab
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-02 14:42:32 +1000

    Order form refactored

--- src/app/pages/widgets-group/order-page/components/sidebar-result/sidebar-result.component.ts
+++ src/app/pages/widgets-group/order-page/components/sidebar-result/sidebar-result.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-result',
   templateUrl: './sidebar-result.component.html',
   styleUrls: ['./sidebar-result.component.scss']
 })
-export class SidebarResultComponent implements OnInit {
+export class SidebarResultComponent implements OnInit, OnChanges {
+  @Input() currentStep;
+
+  public FormStep = {
+    One: 0,
+    Two: 1,
+    Three: 2,
+    Four: 3
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.currentStep = changes.currentStep.currentValue;
+  }
 }
