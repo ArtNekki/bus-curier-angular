673a5c72ca1fea73096d323a1a2b987522ea104f
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-02 10:16:50 +1000

    Modals done

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,14 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ModsService} from '../../../core/services/mods.service';
+import fadeIn from '../../../core/animations/fadeIn';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fadeIn]
 })
 export class ModalComponent implements OnInit {
   @Input() title: string;
   @Input() mods;
+
+  @Output() close: EventEmitter<any> = new EventEmitter<any>();
 
   public cssClass = 'modal';
 
@@ -18,4 +22,7 @@ export class ModalComponent implements OnInit {
     this.cssClass = this.modsService.setMods('modal', this.mods);
   }
 
+  onClose() {
+    this.close.emit();
+  }
 }
