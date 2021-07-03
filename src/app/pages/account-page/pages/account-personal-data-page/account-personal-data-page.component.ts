0ad39f7784485873b166479c8e5a86c74ae0b8e2
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-04 09:08:34 +1000

    Edit logic added

--- src/app/pages/account-page/pages/account-personal-data-page/account-personal-data-page.component.ts
+++ src/app/pages/account-page/pages/account-personal-data-page/account-personal-data-page.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import UserType from '../../../../core/maps/UserType';
+
+const EntityModType = {
+  Active: 'active',
+  EditAuth: 'editAuth',
+  EditCompany: 'editCompany'
+};
+
+const IndividualModType = {
+  Active: 'active',
+  Edit: 'edit'
+};
 
 @Component({
   selector: 'app-account-personal-data-page',
@@ -6,9 +19,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-personal-data-page.component.scss']
 })
 export class AccountPersonalDataPageComponent implements OnInit {
-  public auth = [
+  public authEntity = [
     {name: 'Логин:', value: 'tafibuchgalter@mail.ru'},
     {name: 'Пароль:', value: '********'}];
+
+  public individual = [
+    {name: 'ФИО:', value: 'Петров Иван Александрович'},
+    {name: 'Email:', value: 'tafibuchgalter@mail.ru'},
+    {name: 'Телефон:', value: '+7 (900) 434 34 54'},
+    {name: 'Паспортные данные:', value: '8013 902456'},
+    {name: 'Пароль:', value: '********'}
+  ];
 
   public organization = [
     {name: 'Наименование:', value: 'Тафи-диагностика МЛ, ООО'},
@@ -25,9 +46,41 @@ export class AccountPersonalDataPageComponent implements OnInit {
     {name: 'Перевозчик:', value: '21'}
   ];
 
-  constructor() { }
+  public entityMod = {
+    [EntityModType.Active]: false,
+    [EntityModType.EditAuth]: false,
+    [EntityModType.EditCompany]: false
+  };
+
+  public individualMod = {
+    [IndividualModType.Active]: false,
+    [IndividualModType.Edit]: false
+  };
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params[UserType.Entity]) {
+        this.entityMod[EntityModType.Active] = true;
 
+        if (params[EntityModType.EditAuth]) {
+          this.entityMod[EntityModType.EditAuth] = true;
+          this.entityMod[EntityModType.EditCompany] = false;
+        } else if (params[EntityModType.EditCompany]) {
+          this.entityMod[EntityModType.EditCompany] = true;
+          this.entityMod[EntityModType.EditAuth] = false;
+        }
+
+      } else if (params[UserType.Individual]) {
+        this.individualMod[IndividualModType.Active] = true;
+
+        if (params[IndividualModType.Edit]) {
+          this.individualMod[IndividualModType.Edit] = true;
+        } else {
+          this.individualMod[IndividualModType.Edit] = false;
+        }
+      }
+    });
+  }
 }
