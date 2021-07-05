f3118cae47be5741c0594bb4b394d3c68b76f0ef
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-05 18:38:22 +1000

    Services refactored

--- src/app/pages/services-group/services-page/services-page.component.ts
+++ src/app/pages/services-group/services-page/services-page.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+const links = [
+  {text: 'Поиск заказа', href: '#'},
+  {text: 'Личный кабинет', href: '#'},
+  {text: 'Онлайн-заявка', href: '#'},
+  {text: 'Расчет тарифа по Приморью', href: '#'},
+  {text: 'Расчет тарифа по Республике Татарстан', href: '#'},
+  {text: 'Расчет тарифа по России', href: '#'}
+];
+
 @Component({
   selector: 'app-services-page',
   templateUrl: './services-page.component.html',
   styleUrls: ['./services-page.component.scss']
 })
 export class ServicesPageComponent implements OnInit {
+  public links = links;
 
   constructor() { }
 
