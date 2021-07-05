a724fcca24bc4e3e92416b16449d1a6fb6ae22e9
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-05 22:39:31 +1000

    Support form refactored

--- src/app/shared/components/support-form/support-form.component.ts
+++ src/app/shared/components/support-form/support-form.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import cities from '../../../mock-data/cities';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import formFieldMeta from '../../../core/form/formFieldMeta';
+import fieldError from '../../../core/form/fieldError';
+import {UtilsService} from '../../../core/services/utils.service';
+import FormControlName from 'src/app/core/maps/FormControlName';
+import {FormUtilsService} from '../../../core/services/form-utils.service';
 
 @Component({
   selector: 'app-support-form',
@@ -8,23 +13,32 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./support-form.component.scss']
 })
 export class SupportFormComponent implements OnInit {
+  public FormFieldMeta = formFieldMeta;
+  public FormControlName = FormControlName;
+  public FormFieldError = fieldError;
+
   public cities = cities;
   public form: FormGroup;
 
-  constructor() { }
+  constructor(public formUtils: FormUtilsService,
+              public utils: UtilsService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      city: new FormControl(this.cities[1].value, [Validators.required]),
-      email: new FormControl('', [Validators.required]),
-      tel: new FormControl('', [Validators.required]),
-      question: new FormControl('', [Validators.required]),
-      subscribe: new FormControl('', []),
+      [FormControlName.Location]: new FormControl('', [Validators.required]),
+      [FormControlName.Email]: new FormControl('', [Validators.required, Validators.email]),
+      [FormControlName.Tel]: new FormControl('', [Validators.required]),
+      [FormControlName.Question]: new FormControl('', [Validators.required]),
+      [FormControlName.Agree]: new FormControl('', []),
     });
+
+    cities.unshift({value: '', name: 'Не выбрано'});
+    this.cities = cities;
   }
 
   onSubmit() {
+    this.form.markAllAsTouched();
     const formData = this.form;
-    console.log('this.form', formData);
+    console.log('support form', formData.value);
   }
 }
