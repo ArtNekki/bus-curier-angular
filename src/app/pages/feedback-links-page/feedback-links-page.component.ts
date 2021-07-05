62fdfa835094d286107777f95c97700c2167c6e6
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-05 18:25:14 +1000

    Feedback-links page done

--- src/app/pages/feedback-links-page/feedback-links-page.component.ts
+++ src/app/pages/feedback-links-page/feedback-links-page.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+const links = [
+  {text: 'Как отправить посылку', href: '#'},
+  {text: 'Как получить посылку', href: '#'},
+  {text: 'Правила приемки и отправки грузов', href: '#'},
+  {text: 'Тарифы на перевозку', href: '#'},
+  {text: 'Упаковки грузов и виды упаковки', href: '#'},
+  {text: 'Хранение груза', href: '#'},
+  {text: 'Доставка грузов и багажа из Аэропорта', href: '#'}
+];
+
 @Component({
   selector: 'app-feedback-links-page',
   templateUrl: './feedback-links-page.component.html',
   styleUrls: ['./feedback-links-page.component.scss']
 })
 export class FeedbackLinksPageComponent implements OnInit {
+  public links = links;
 
   constructor() { }
 
--- src/app/pages/feedback-links-page/feedback-links-page.module.ts
+++ src/app/pages/feedback-links-page/feedback-links-page.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FeedbackLinksPageComponent} from './feedback-links-page.component';
+import {SharedModule} from '../../shared/shared.module';
 
 
 
@@ -9,7 +10,8 @@ import {FeedbackLinksPageComponent} from './feedback-links-page.component';
     FeedbackLinksPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class FeedbackLinksPageModule { }
