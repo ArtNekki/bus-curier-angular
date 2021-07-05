1eb5fc42e69f5a710df099b1e50c384dad67d6fa
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-05 14:29:27 +1000

    About page refactored

--- src/app/pages/about-page/components/team/team.component.ts
+++ src/app/pages/about-page/components/team/team.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+const list = [
+  'проводим планерки',
+  'совместные выезды',
+  'обучение и тренинги',
+  'интересные и яркие корпоративы'
+];
+
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
+  public list = list;
 
   constructor() { }
 
