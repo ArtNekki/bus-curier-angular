5063c15950f9565c8bad2ad783e5bb3eff03e64c
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-07-05 20:21:10 +1000

    Cargo info refactored

--- src/app/pages/info-group/transportation-rates-page/transportation-rates-page.component.ts
+++ src/app/pages/info-group/transportation-rates-page/transportation-rates-page.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import City from 'src/app/core/maps/City';
+import fadeIn from '../../../core/animations/fadeIn';
 
 @Component({
   selector: 'app-transportation-rates-page',
   templateUrl: './transportation-rates-page.component.html',
-  styleUrls: ['./transportation-rates-page.component.scss']
+  styleUrls: ['./transportation-rates-page.component.scss'],
+  animations: [fadeIn]
 })
 
 export class TransportationRatesPageComponent implements OnInit {
+  public City = City;
+
+  public currentCity;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.currentCity = City.Vladivostok;
   }
 
   setCurrentCity(id: any) {
-    console.log('cityId', id);
+    this.currentCity = id;
   }
 }
