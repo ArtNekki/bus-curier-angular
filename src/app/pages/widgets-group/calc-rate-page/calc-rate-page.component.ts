6b8b956b7509995f257e117c58d327f7069009ec
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-08-09 11:34:03 +1000

    calc-rate page updated

--- src/app/pages/widgets-group/calc-rate-page/calc-rate-page.component.ts
+++ src/app/pages/widgets-group/calc-rate-page/calc-rate-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import cities from 'src/app/mock-data/cities';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import FormControlName from '../../../core/maps/FormControlName';
 
 @Component({
   selector: 'app-calc-rate-page',
@@ -7,12 +9,24 @@ import cities from 'src/app/mock-data/cities';
   styleUrls: ['./calc-rate-page.component.scss']
 })
 export class CalcRatePageComponent implements OnInit {
+  public FormControlName = FormControlName;
 
+  public form: FormGroup;
   public cities = cities;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      [FormControlName.DeparturePoint]: new FormControl('', [Validators.required]),
+      [FormControlName.PickupPoint]: new FormControl('', [Validators.required]),
+      ['cargo-group']: new FormControl('', [Validators.required]),
+      [FormControlName.Packaging]: new FormControl(''),
+      [FormControlName.Services]: new FormControl('')
+    });
   }
 
+  onSubmit() {
+    console.log('quick order', this.form.value);
+  }
 }
--- src/app/pages/widgets-group/calc-rate-page/calc-rate-page.module.ts
+++ src/app/pages/widgets-group/calc-rate-page/calc-rate-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CalcRatePageComponent} from './calc-rate-page.component';
 import {SharedModule} from '../../../shared/shared.module';
+import {OrderFormModule} from '../common-modules/order-form/order-form.module';
 
 
 
@@ -11,7 +12,8 @@ import {SharedModule} from '../../../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    OrderFormModule
   ]
 })
 export class CalcRatePageModule { }
