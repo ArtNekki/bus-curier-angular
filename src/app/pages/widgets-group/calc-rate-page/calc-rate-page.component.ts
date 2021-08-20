01dc475b9d16e5eade26e5a9b18eba20bef4dcef
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-08-20 13:07:12 +1000

    city loading added

--- src/app/pages/widgets-group/calc-rate-page/calc-rate-page.component.ts
+++ src/app/pages/widgets-group/calc-rate-page/calc-rate-page.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import cities from 'src/app/mock-data/cities';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import FormControlName from '../../../core/maps/FormControlName';
+import {delay, switchMap, tap} from 'rxjs/operators';
+import Service from '../../../core/models/Service';
+import {OrderFormService} from '../../../core/services/order-form/order-form.service';
+import {CalculatorService} from '../../../core/services/calculator/calculator.service';
 
 @Component({
   selector: 'app-calc-rate-page',
@@ -13,8 +17,10 @@ export class CalcRatePageComponent implements OnInit {
 
   public form: FormGroup;
   public cities = cities;
+  public cityId: string;
+  public loading = false;
 
-  constructor() { }
+  constructor(protected orderForm: OrderFormService, private calcService: CalculatorService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -23,6 +29,17 @@ export class CalcRatePageComponent implements OnInit {
       ['cargo-group']: new FormControl('', [Validators.required]),
       [FormControlName.Packaging]: new FormControl(''),
       [FormControlName.Services]: new FormControl('')
+    });
+
+    this.orderForm.cityFrom$.pipe(
+      tap(() => {
+        this.loading = true;
+        this.cityId = null;
+      }),
+      delay(1000)
+    ).subscribe((id: string) => {
+      this.loading = false;
+      this.cityId = id;
     });
   }
 
