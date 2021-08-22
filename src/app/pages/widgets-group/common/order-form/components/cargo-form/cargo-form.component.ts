7668d928d3239cc664deab72c46f2c7008b00d4c
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-08-22 16:55:51 +1000

    Cargo refactored

--- src/app/pages/widgets-group/common/order-form/components/cargo-form/cargo-form.component.ts
+++ src/app/pages/widgets-group/common/order-form/components/cargo-form/cargo-form.component.ts
@@ -1,6 +1,6 @@
-import {ChangeDetectorRef, Component, forwardRef, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, OnInit} from '@angular/core';
 import formFieldMeta from '../../../../../../core/form/formFieldMeta';
-import {FormArray, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
 import {FormUtilsService} from '../../../../../../core/services/form-utils.service';
 import {UtilsService} from '../../../../../../core/services/utils.service';
 import {SimpleModalService} from 'ngx-simple-modal';
@@ -9,6 +9,11 @@ import {ConfirmModalComponent} from '../../../../../../modals/confirm-modal/conf
 import {SubFormComponent} from '../sub-form/sub-form.component';
 import FormControlName from 'src/app/core/maps/FormControlName';
 import fadeIn from '../../../../../../core/animations/fadeIn';
+import CargoType from '../../../../../../core/models/CargoType';
+import Select from '../../../../../../core/models/Select';
+import {Subscription} from 'rxjs';
+import {CalculatorService} from '../../../../../../core/services/calculator/calculator.service';
+import {delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-cargo-form',
@@ -26,19 +31,29 @@ import fadeIn from '../../../../../../core/animations/fadeIn';
       useExisting: forwardRef(() => CargoFormComponent),
       multi: true
     }
-  ],
+  ]
 })
 export class CargoFormComponent extends SubFormComponent implements OnInit  {
+  public Cargo = {
+    Docs: '1',
+    Parcels: '2',
+    AutoParts: '5',
+    Other: '21'
+  };
+
   public FormControlName = FormControlName;
   public FormFieldMeta = formFieldMeta;
 
   public formGroup: FormGroup;
   public currentCargoType = null;
+  // public items: Array<Select> = [];
+  public itemsSub: Subscription;
   public isOk = false;
 
   constructor(public formUtils: FormUtilsService,
               public utils: UtilsService,
               private cdr: ChangeDetectorRef,
+              private calcService: CalculatorService,
               private simpleModal: SimpleModalService,
               orderForm: OrderFormService) {
     super(orderForm);
@@ -46,18 +61,31 @@ export class CargoFormComponent extends SubFormComponent implements OnInit  {
 
   ngOnInit(): void {
     this.formGroup = new FormGroup({
-      activeItem: new FormControl(FormControlName.Docs),
-      items: new FormGroup({
-        [FormControlName.Docs]: new FormControl(''),
-        [FormControlName.Parcels]: new FormControl(''),
-        [FormControlName.AutoParts]: new FormControl(''),
-        [FormControlName.Other]: new FormControl('')
-      })
+      activeItem: new FormControl(this.Cargo.Docs),
+      items: new FormGroup({}, [Validators.required])
     });
 
-    this.formGroup.markAllAsTouched();
-
     super.ngOnInit();
+
+    this.itemsSub = this.calcService.getTypes(1, 1)
+      .pipe(delay(500))
+      .subscribe((result: Array<CargoType>) => {
+        if (result.length) {
+
+          result.forEach((item: CargoType) => {
+            if (item.parent_id === '0') {
+              this.items.addControl(item.id, new FormControl(''));
+            }
+          });
+        }
+
+        this.cdr.detectChanges();
+        this.formGroup.reset(this.formGroup.value);
+    });
+  }
+
+  get items() {
+    return this.formGroup.get('items') as FormGroup;
   }
 
   get cargo() {
@@ -92,52 +120,51 @@ export class CargoFormComponent extends SubFormComponent implements OnInit  {
     setTimeout(() => {
 
       switch (type) {
-        case FormControlName.Docs:
+        case this.Cargo.Docs:
           this.formGroup.get('items')
-            .get(FormControlName.Parcels)
+            .get(this.Cargo.Parcels)
             .patchValue('', {onlySelf: true});
           this.formGroup.get('items')
-            .get(FormControlName.AutoParts)
+            .get(this.Cargo.AutoParts)
             .patchValue('', {onlySelf: true});
           this.formGroup.get('items')
-            .get(FormControlName.Other)
-            .patchValue('', {onlySelf: true});
-          break;
-        case FormControlName.Parcels:
-          this.formGroup.get('items')
-            .get(FormControlName.Docs)
-            .setValue('', {onlySelf: true});
-          this.formGroup.get('items')
-            .get(FormControlName.AutoParts)
-            .setValue('', {onlySelf: true});
-          this.formGroup.get('items')
-            .get(FormControlName.Other)
+            .get(this.Cargo.Other)
             .patchValue('', {onlySelf: true});
           break;
-        case FormControlName.AutoParts:
+        case this.Cargo.Parcels:
+          this.formGroup.get('items')
+            .get(this.Cargo.Docs)
+            .setValue('', {onlySelf: true});
+          this.formGroup.get('items')
+            .get(this.Cargo.AutoParts)
+            .setValue('', {onlySelf: true});
+          this.formGroup.get('items')
+            .get(this.Cargo.Other)
+            .patchValue('', {onlySelf: true});
+          break;
+        case this.Cargo.AutoParts:
           this.formGroup
-            .get('items').get(FormControlName.Parcels)
+            .get('items').get(this.Cargo.Parcels)
             .setValue('', {onlySelf: true});
           this.formGroup.get('items')
-            .get(FormControlName.Docs)
+            .get(this.Cargo.Docs)
             .setValue('', {onlySelf: true});
           this.formGroup.get('items')
-            .get(FormControlName.Other)
+            .get(this.Cargo.Other)
             .patchValue('', {onlySelf: true});
           break;
-        case FormControlName.Other:
+        case this.Cargo.Other:
           this.formGroup
-            .get('items').get(FormControlName.Parcels)
+            .get('items').get(this.Cargo.Parcels)
             .setValue('', {onlySelf: true});
           this.formGroup.get('items')
-            .get(FormControlName.Docs)
+            .get(this.Cargo.Docs)
             .setValue('', {onlySelf: true});
           this.formGroup.get('items')
-            .get(FormControlName.AutoParts)
+            .get(this.Cargo.AutoParts)
             .patchValue('', {onlySelf: true});
           break;
       }
-
     }, 0);
   }
 
