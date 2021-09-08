812873eef333d2c7bacced83ddb17489c6698b10
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-09-08 13:50:53 +1000

    Order-form component added

--- src/app/pages/orders-group/common/order-form/components/order-form/order-form.component.ts
+++ src/app/pages/orders-group/common/order-form/components/order-form/order-form.component.ts
@@ -1,15 +1,191 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, forwardRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import CargoType from '../../../../../../core/models/CargoType';
+import Service from '../../../../../../core/models/Service';
+import formFieldMeta from '../../../../../../core/form/formFieldMeta';
+import {FormArray, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {FormUtilsService} from '../../../../../../core/services/form-utils.service';
+import {UtilsService} from '../../../../../../core/services/utils.service';
+import {CalculatorService} from '../../../../../../core/services/calculator/calculator.service';
+import {SimpleModalService} from 'ngx-simple-modal';
+import {OrderFormService} from '../../../../../../core/services/order-form/order-form.service';
+import {delay} from 'rxjs/operators';
+import {ConfirmModalComponent} from '../../../../../../modals/confirm-modal/confirm-modal.component';
+import {SubFormComponent} from '../sub-form/sub-form.component';
+import fadeIn from '../../../../../../core/animations/fadeIn';
+import FormControlName from 'src/app/core/maps/FormControlName';
 
 @Component({
   selector: 'app-order-form',
   templateUrl: './order-form.component.html',
-  styleUrls: ['./order-form.component.scss']
+  styleUrls: ['./order-form.component.scss'],
+  animations: [fadeIn],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => OrderFormComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => OrderFormComponent),
+      multi: true
+    }
+  ]
 })
-export class OrderFormComponent implements OnInit {
+export class OrderFormComponent extends SubFormComponent implements OnInit, OnChanges {
+  @Input() cityFromId: string;
+  @Input() types: Array<CargoType>;
+  @Input() services: Array<Service>;
 
-  constructor() { }
+  public Cargo = {
+    Docs: '1',
+    Parcels: '2',
+    AutoParts: '5',
+    Other: '21'
+  };
 
-  ngOnInit(): void {
+  public CargoName = {
+    1: 'Документы',
+    2: 'Посылки',
+    5: 'Автозапчасти',
+    21: 'Другое'
+  };
+
+  public FormControlName = FormControlName;
+  public FormFieldMeta = formFieldMeta;
+
+  public formGroup: FormGroup;
+  public currentCargoType = null;
+  // public items: Array<Select> = [];
+  public itemsSub: Subscription;
+  public isOk = false;
+
+  constructor(public formUtils: FormUtilsService,
+              public utils: UtilsService,
+              private cdr: ChangeDetectorRef,
+              private calcService: CalculatorService,
+              private simpleModal: SimpleModalService,
+              orderForm: OrderFormService) {
+    super(orderForm);
   }
 
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      activeItem: new FormControl(this.Cargo.Docs),
+      items: new FormGroup({}, [Validators.required]),
+      [FormControlName.Packaging]: new FormControl(''),
+      [FormControlName.Services]: new FormControl('')
+    });
+
+    super.ngOnInit();
+
+    if (this.types.length) {
+      this.setTypes(this.types, false);
+    }
+
+    this.formGroup.get('activeItem').valueChanges
+      .pipe(delay(10))
+      .subscribe((id: string) => {
+
+        switch (id) {
+          case this.Cargo.Docs:
+            this.items
+              .get(this.Cargo.Parcels)
+              .patchValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.AutoParts)
+              .patchValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.Other)
+              .patchValue('', {onlySelf: true});
+            break;
+          case this.Cargo.Parcels:
+            this.items
+              .get(this.Cargo.Docs)
+              .setValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.AutoParts)
+              .setValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.Other)
+              .patchValue('', {onlySelf: true});
+            break;
+          case this.Cargo.AutoParts:
+            this.items
+              .get(this.Cargo.Parcels)
+              .setValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.Docs)
+              .setValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.Other)
+              .patchValue('', {onlySelf: true});
+            break;
+          case this.Cargo.Other:
+            this.items
+              .get(this.Cargo.Parcels)
+              .setValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.Docs)
+              .setValue('', {onlySelf: true});
+            this.items
+              .get(this.Cargo.AutoParts)
+              .patchValue('', {onlySelf: true});
+            break;
+        }
+      });
+  }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.types && changes.types.currentValue.length  && !changes.types.firstChange) {
+      this.setTypes(changes.types.currentValue, true);
+    }
+  }
+
+  get items() {
+    return this.formGroup.get('items') as FormGroup;
+  }
+
+  get cargo() {
+    return this.formGroup.get(FormControlName.Cargo) as FormArray;
+  }
+
+  setCargoType(e, type: string) {
+
+    if (!e.preventDefault) {
+      return;
+    }
+
+    this.confirm().subscribe((result) => {
+
+      if (!result) {
+        return;
+      }
+
+      this.formGroup.get('activeItem').setValue(type);
+
+    });
+  }
+
+  setTypes(types, onChanges) {
+    const cargoTypes = types.filter((item: CargoType) => item.parent_id === '0' && item)
+      .map((item: CargoType) => ({id: item.id, name: item.name}));
+
+    cargoTypes.forEach((item: CargoType) => {
+      this.items.addControl(item.id, new FormControl(''));
+    });
+
+    if (onChanges) {
+      this.cdr.detectChanges();
+      this.formGroup.reset(this.formGroup.value);
+    }
+  }
+
+  confirm() {
+    return this.simpleModal.addModal(ConfirmModalComponent, {
+      message: `Вы уверены? <br /> Данные этого груза будут потеряны.`
+    });
+  }
 }
