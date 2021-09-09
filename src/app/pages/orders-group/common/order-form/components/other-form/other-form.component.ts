c80137b03219f4be0a0cd548baa1730428b8d2ba
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-09-09 13:35:24 +1000

    Other-form refactored

--- src/app/pages/orders-group/common/order-form/components/other-form/other-form.component.ts
+++ src/app/pages/orders-group/common/order-form/components/other-form/other-form.component.ts
@@ -10,11 +10,14 @@ import {Subscription} from 'rxjs';
 import Select from 'src/app/core/models/Select';
 import CargoType from '../../../../../../core/models/CargoType';
 import {CalculatorService} from '../../../../../../core/services/calculator/calculator.service';
+import {FormUtilsService} from '../../../../../../core/services/form-utils.service';
+import fadeIn from '../../../../../../core/animations/fadeIn';
 
 @Component({
   selector: 'app-other-form',
   templateUrl: './other-form.component.html',
   styleUrls: ['./other-form.component.scss'],
+  animations: [fadeIn],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -37,6 +40,7 @@ export class OtherFormComponent extends SubFormComponent implements OnInit, OnCh
   public parts: Array<Select> = [];
 
   constructor(
+    public formUtils: FormUtilsService,
     private cdr: ChangeDetectorRef,
     private simpleModal: SimpleModalService,
     private calcService: CalculatorService,
@@ -46,9 +50,8 @@ export class OtherFormComponent extends SubFormComponent implements OnInit, OnCh
 
   ngOnInit(): void {
     this.formGroup = new FormGroup({
-      items: new FormArray([
-        new FormControl('', [Validators.required])
-      ])
+      [FormControlName.Item]: new FormControl('', [Validators.required]),
+      [FormControlName.Counter]: new FormControl('', [Validators.required])
     });
 
     this.setParts(this.types);
@@ -64,42 +67,18 @@ export class OtherFormComponent extends SubFormComponent implements OnInit, OnCh
   }
 
   setParts(arr: Array<CargoType>) {
-    this.parts = arr.filter((item: CargoType) => item.parent_id === '21' && item)
-      .map((item: CargoType) => ({value: item.id, name: item.name}));
-  }
-
-  public get items(): FormArray {
-    return this.formGroup.get('items') as FormArray;
-  }
-
-  get isSomeItemsInvalid() {
-    return this.items.controls.some((control) => {
-      return control.invalid;
-    });
-  }
-
-  add() {
-    this.items.push(new FormControl('', [Validators.required]));
-    this.formGroup.markAllAsTouched();
-  }
-
-  delete(index: number) {
-    if (this.items.length <= 1) {
-      return;
-    }
-
-    if (this.items.value[index]) {
-      this.confirm().subscribe((ok) => {
-        if (!ok) {
-          return;
-        }
-
-        this.items.removeAt(index);
-        this.cdr.detectChanges();
+    const parts = arr.filter((item: CargoType) => item.parent_id === '21' && item)
+      .map((item: CargoType) => {
+        return {value: item.id, name: item.name};
       });
-    } else {
-      this.items.removeAt(index);
-    }
+
+    this.parts = [{value: '', name: ''}, ...parts];
+  }
+
+  clear() {
+    this.formGroup.get(FormControlName.Item).setValue('');
+    this.formGroup.get(FormControlName.Item).enable();
+    this.formGroup.get(FormControlName.Counter).setValue('');
   }
 
   confirm() {
@@ -108,14 +87,7 @@ export class OtherFormComponent extends SubFormComponent implements OnInit, OnCh
     });
   }
 
-  writeValue(value: any): void {
-    if (value) {
-      this.items.clear();
-      value.forEach(item => this.items.push(new FormControl(item, [Validators.required])));
-    }
-  }
-
-  registerOnChange(fn: any): void {
-    this.formGroup.valueChanges.pipe( map(value => value.items)).subscribe(fn);
+  disableSelect(control) {
+    // control.disable({onlySelf: false, emitEvent: false});
   }
 }
