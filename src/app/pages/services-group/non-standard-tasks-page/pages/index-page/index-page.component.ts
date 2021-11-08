4b1cbb5f237b3fc00111180fccc746c41c1fadce
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-11-08 22:26:46 +1000

    Manager call done

--- src/app/pages/services-group/non-standard-tasks-page/pages/index-page/index-page.component.ts
+++ src/app/pages/services-group/non-standard-tasks-page/pages/index-page/index-page.component.ts
@@ -4,6 +4,11 @@ import fieldError from '../../../../../core/form/fieldError';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UtilsService} from '../../../../../core/services/utils.service';
 import FormControlName from 'src/app/core/maps/FormControlName';
+import {SimpleModalService} from 'ngx-simple-modal';
+import { CommonService } from 'src/app/core/services/common/common.service';
+import {environment} from '../../../../../../environments/environment';
+import {take} from 'rxjs/operators';
+import {AlertModalComponent} from '../../../../../modals/alert-modal/alert-modal.component';
 
 @Component({
   selector: 'app-index-page',
@@ -16,19 +21,74 @@ export class IndexPageComponent implements OnInit, OnDestroy {
   public FormFieldError = fieldError;
 
   public form: FormGroup;
+  public isLoading = false;
 
-  constructor(public utils: UtilsService) { }
+  constructor(
+    public utils: UtilsService,
+    private simpleModal: SimpleModalService,
+    private commonService: CommonService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      [FormControlName.Tel]: new FormControl('', [Validators.required]),
-      [FormControlName.Agree]: new FormControl('')
+      [FormControlName.Tel]: new FormControl('',
+        [Validators.required, Validators.pattern(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/)]),
+      [FormControlName.Agree]: new FormControl(''),
+      [FormControlName.Captcha]: new FormControl('', [Validators.required])
     });
 
     document.body.classList.add('non-standard-tasks');
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.form.markAllAsTouched();
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    const data = this.form.value;
+
+    this.commonService.sendMail({
+      'api-key': environment.api_key,
+      phone: data.tel
+    })
+      .pipe(take(1))
+      .subscribe(() => {
+          this.isLoading = false;
+          this.alert('Ваше сообщение отправлено!<br /> Мы свяжемся с Вами в ближайшее время!')
+            .pipe(take(1))
+            .subscribe(() => {
+              this.form.reset();
+            });
+        },
+        (error) => {
+          this.isLoading = false;
+          this.alert('Ой, что-то пошло не так!<br /> Сообщение не было отправлено!', 'Понятно!')
+            .pipe(take(1))
+            .subscribe(() => {});
+        });
+  }
+
+  captchaResolved(value: string) {
+
+    if (!value) {
+      this.form.get(FormControlName.Agree).setValue(false);
+    }
+  }
+
+  executeCaptcha(captcha) {
+    captcha.execute();
+    this.form.markAllAsTouched();
+  }
+
+  alert(message, btnText = null) {
+    return this.simpleModal.addModal(AlertModalComponent, {
+      message,
+      btnText
+    });
+  }
 
   ngOnDestroy(): void {
     document.body.classList.remove('non-standard-tasks');
