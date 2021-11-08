a788515aea6fb49225262534c6999b30a20b94a9
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-11-08 15:06:05 +1000

    How to get and how to send pages refactored

--- src/app/pages/info-group/how-to-get-page/how-to-get-page.component.ts
+++ src/app/pages/info-group/how-to-get-page/how-to-get-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {of, Subscription, zip} from 'rxjs';
+import {SimpleModalService} from 'ngx-simple-modal';
+import { CalculatorService } from 'src/app/core/services/calculator/calculator.service';
+import {CitiesModalComponent} from '../../../modals/cities-modal/cities-modal.component';
 
 @Component({
   selector: 'app-how-to-get-page',
   templateUrl: './how-to-get-page.component.html',
   styleUrls: ['./how-to-get-page.component.scss']
 })
-export class HowToGetPageComponent implements OnInit {
+export class HowToGetPageComponent implements OnInit, OnDestroy {
+  private cities: any;
+  private sub: Subscription;
 
-  constructor() { }
+  constructor(private calcService: CalculatorService,
+              private modalService: SimpleModalService) { }
 
   ngOnInit(): void {
+
+    this.sub = this.calcService.getCityTo(1, 0)
+      .subscribe((cities) => {
+        this.cities = cities;
+      });
   }
 
+  showCitiesModal(e) {
+    e.preventDefault();
+    this.modalService.addModal(CitiesModalComponent, {
+      title: 'Населенные пункты получения',
+      list: of(this.cities)
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
--- src/app/pages/info-group/how-to-send-page/how-to-send-page.component.ts
+++ src/app/pages/info-group/how-to-send-page/how-to-send-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {of, Subscription} from 'rxjs';
+import {SimpleModalService} from 'ngx-simple-modal';
+import { CalculatorService } from 'src/app/core/services/calculator/calculator.service';
+import {CitiesModalComponent} from '../../../modals/cities-modal/cities-modal.component';
 
 @Component({
   selector: 'app-how-to-send-page',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./how-to-send-page.component.scss']
 })
 export class HowToSendPageComponent implements OnInit {
+  private cities: any;
+  private sub: Subscription;
 
-  constructor() { }
+  constructor(private calcService: CalculatorService,
+              private modalService: SimpleModalService) { }
 
   ngOnInit(): void {
+    this.sub = this.calcService.getCitiesFrom()
+      .subscribe((cities) => {
+        this.cities = cities;
+      });
+  }
+
+  showCitiesModal(e) {
+    e.preventDefault();
+    this.modalService.addModal(CitiesModalComponent, {
+      title: 'Населенные пункты отправления',
+      list: of(this.cities)
+    });
   }
 
 }
