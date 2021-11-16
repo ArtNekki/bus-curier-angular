edd4c970962620e20df7c48f247d061da25d9d0f
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-11-16 18:46:25 +1000

    link refactored

--- src/app/pages/services-group/courier-page/courier-page.component.ts
+++ src/app/pages/services-group/courier-page/courier-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 
 
@@ -16,6 +17,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courier-page.component.scss']
 })
 export class CourierPageComponent implements OnInit {
+  public phoneNumber = '';
   public cities = [
     'Владивосток (забор и доставка груза)',
     'Уссурийск (забор и доставка груза)',
@@ -26,9 +28,10 @@ export class CourierPageComponent implements OnInit {
     'Хабаровск (забор и доставка)'
   ];
 
-  constructor() { }
+  constructor(private localStorage: LocalStorageService) { }
 
   ngOnInit(): void {
+    this.phoneNumber = this.localStorage.get('phone-number');
   }
 
 }
